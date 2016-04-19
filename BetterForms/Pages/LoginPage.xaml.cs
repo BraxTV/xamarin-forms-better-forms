b797d20bae5467757b8a297d9f6578bfc21918e1
Ben Bishop <ben@rendr.io>
2016-04-19 09:29:55 -0500

    Inital lesson setup: - Created UI - Created simple VM - Created Xaml bindings to VM

---
+++ BetterForms/Pages/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using BetterForms.ViewModels;
+
+namespace BetterForms.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            var vm = new LoginViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Sorry", "The credentials you supplied are incorrect.", "Ok");
+            InitializeComponent();
+
+           
+        }
+    }
+}
+
---
+++ BetterForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace BetterForms.ViewModels
+{
+    public class LoginViewModel:INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        #endregion
+
+        public Action DisplayInvalidLoginPrompt;
+
+        private string emailAddress;
+
+        public string EmailAddress
+        {
+            get
+            {
+                return emailAddress;
+            }
+            set
+            {
+                emailAddress = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("EmailAddress"));
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+
+        public ICommand SubmitCommand{ protected set; get; }
+
+        public LoginViewModel()
+        {
+            SubmitCommand = new Command(OnSubmit);
+        }
+
+        public void OnSubmit()
+        {
+            if (emailAddress != "ben@brax.tv" || password != "letmein")
+                DisplayInvalidLoginPrompt();
+        }
+    }
+}
+
