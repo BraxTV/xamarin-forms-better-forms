531e73e82406d80fe3f22a5b0a23bf39029ee049
Ben Bishop <ben@rendr.io>
2016-05-23 09:46:55 -0500

    Added RegexUtil.

---
+++ BetterForms/Utils/RegexUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BetterForms.Utils
+{
+    public static class RegexUtil
+    {
+        public static Regex ValidEmailAddress()
+        {
+            return new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        }
+
+        public static Regex MinLength(int length)
+        {
+            return new Regex(@"(\s*(\S)\s*){" + length + @",}");
+        }
+
+        public static Regex ValidZipCode()
+        {
+            return new Regex(@"^\d{5}(?:[-\s]\d{4})?$");
+        }
+    }
+}
+
