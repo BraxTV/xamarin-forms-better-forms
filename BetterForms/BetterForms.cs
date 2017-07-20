9c6838a3a9b35dd65ba7aace13a59ae972d90134
Benjamin Bishop <ben@rendr.io>
2017-07-20 14:51:58 -0500

    Making sure latest is in.

--- BetterForms/BetterForms.cs
+++ BetterForms/BetterForms.cs
@@ -2,28 +2,31 @@
 
 using Xamarin.Forms;
 using BetterForms.Pages;
+using XLabs.Forms.Validation;
 
 namespace BetterForms
 {
     public class App : Application
     {
-        public App()
+        public App ()
         {
             // The root page of your application
-            MainPage = new LoginPage();
+            MainPage = new LoginPage ();
+
+            new Validator ();
         }
 
-        protected override void OnStart()
+        protected override void OnStart ()
         {
             // Handle when your app starts
         }
 
-        protected override void OnSleep()
+        protected override void OnSleep ()
         {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume()
+        protected override void OnResume ()
         {
             // Handle when your app resumes
         }
--- BetterForms/Pages/LoginPage.xaml.cs
+++ BetterForms/Pages/LoginPage.xaml.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 using BetterForms.ViewModels;
+using XLabs.Forms.Validation;
+using System.Diagnostics;
 
 namespace BetterForms.Pages
 {
     public partial class LoginPage : ContentPage
     {
-        public LoginPage()
+        public LoginPage ()
         {
-            var vm = new LoginViewModel();
+            var vm = new LoginViewModel ();
             this.BindingContext = vm;
-            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Sorry", "The credentials you supplied are incorrect.", "Ok");
-            InitializeComponent();
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert ("Sorry", "The credentials you supplied are incorrect.", "Ok");
+            InitializeComponent ();
 
-			EmailEntry.Completed += (object sender, EventArgs e) => {
-				PasswordEntry.Focus();
-			};
+            EmailEntry.Completed += (object sender, EventArgs e) => {
+                PasswordEntry.Focus ();
+            };
 
-			PasswordEntry.Completed += (object sender, EventArgs e) => {
-				vm.SubmitCommand.Execute(null);
-			};
+            PasswordEntry.Completed += (object sender, EventArgs e) => {
+                vm.SubmitCommand.Execute (null);
+            };
+
+
+
         }
     }
 }
